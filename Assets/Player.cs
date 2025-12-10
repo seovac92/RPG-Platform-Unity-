93dad639880616b8635923c99cb6b8bf4de0bfcf
Marko Seovac <seovacmarko5@gmail.com>
2025-12-09 22:09:34 -0800

    basic attack combo on a halfway, it means that it needs more impruvement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -15,8 +15,9 @@ public class Player : MonoBehaviour
     public Player_DashState dashState { get; private set; }
     public Player_BasicAttackState basicAttackState { get; private set; }
     [Header("Attack details")]
-    public Vector2 attackVelocity;
+    public Vector2[] attackVelocity;
     public float attackVelocityDuration = .1f;
+    public float comboResetTime = 1f;
     [Header("Movement details")]
     public float moveSpeed;
     public float jumpForce = 5;
--- Assets/Player_BasicAttackState.cs
+++ Assets/Player_BasicAttackState.cs
@@ -3,13 +3,25 @@ using UnityEngine;
 public class Player_BasicAttackState : EntityState
 {
     private float attackVelocityTimer;
+    private const int FirstComboIndex = 1;
+    private int comboIndex = FirstComboIndex;
+    private int comboLimit = 3;
+    private float lastTimeAttacked;
     public Player_BasicAttackState(Player player, StateMachine stateMachine, string animBoolName) : base(player, stateMachine, animBoolName)
     {
+        if (comboLimit != player.attackVelocity.Length)
+        {
+            comboLimit = player.attackVelocity.Length;
+        }
     }
     public override void Enter()
     {
         base.Enter();
-        GenerateAttackVelocity();
+
+        ResetComboIndexIfNeeded();
+
+        anim.SetInteger("basicAttackIndex", comboIndex);
+        ApplyAttackVelocity();
     }
     public override void Update()
     {
@@ -21,6 +33,13 @@ public class Player_BasicAttackState : EntityState
             stateMachine.ChangeState(player.idleState);
         }
     }
+    public override void Exit()
+    {
+        base.Exit();
+
+        comboIndex++;
+        lastTimeAttacked = Time.time;
+    }
     private void HandleAttackVelocity()
     {
         attackVelocityTimer -= Time.deltaTime;
@@ -29,9 +48,23 @@ public class Player_BasicAttackState : EntityState
             player.SetVelocity(0, rb.linearVelocity.y);
         }
     }
-    private void GenerateAttackVelocity()
+    private void ApplyAttackVelocity()
     {
+        Vector2 attackVelocity = player.attackVelocity[comboIndex - 1];
+
         attackVelocityTimer = player.attackVelocityDuration;
-        player.SetVelocity(player.attackVelocity.x * player.facingDir, player.attackVelocity.y);
+        player.SetVelocity(attackVelocity.x * player.facingDir, attackVelocity.y);
+    }
+    private void ResetComboIndexIfNeeded()
+    {
+        if (lastTimeAttacked + player.comboResetTime < Time.time)
+        {
+            comboIndex = FirstComboIndex;
+        }
+
+        if (comboIndex > comboLimit)
+        {
+            comboIndex = FirstComboIndex;
+        }
     }
 }
