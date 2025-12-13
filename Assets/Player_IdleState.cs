8e79040264ff94e982c9cd81a6a0c3f6c7823ec3
Marko Seovac <seovacmarko5@gmail.com>
2025-12-12 21:52:22 -0800

    fix before moving to another section

--- Assets/Player_IdleState.cs
+++ Assets/Player_IdleState.cs
@@ -15,6 +15,9 @@ public class Player_IdleState : Player_GroundedState
     {
         base.Update();
 
+        if (player.moveInput.x == player.facingDir && player.wallDetected)
+            return;
+
         if (player.moveInput.x != 0)
         {
             stateMachine.ChangeState(player.moveState);
--- Assets/Player_MoveState.cs
+++ Assets/Player_MoveState.cs
@@ -10,7 +10,7 @@ public class Player_MoveState : Player_GroundedState
     {
         base.Update();
 
-        if (player.moveInput.x == 0)
+        if (player.moveInput.x == 0 || player.wallDetected)
         {
             stateMachine.ChangeState(player.idleState);
         }
