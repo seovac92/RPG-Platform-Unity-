c85e93ed6f95847db6219100eaa89b28cf0b5c58
Marko Seovac <seovacmarko5@gmail.com>
2025-12-25 13:49:07 -0800

    looping background

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -4,21 +4,35 @@ public class ParallaxBackground : MonoBehaviour
 {
     private Camera mainCamera;
     private float lastCameraPositionX;
+    private float cameraHalfWidth;
     [SerializeField] private ParallaxLayer[] backgroundLayers;
 
     private void Awake()
     {
         mainCamera = Camera.main;
+        cameraHalfWidth = mainCamera.orthographicSize * mainCamera.aspect;
+        CalculateImageLength();
     }
-    private void Update()
+    private void FixedUpdate()
     {
         float currentCameraPositionX = mainCamera.transform.position.x;
         float distanceToMove = currentCameraPositionX - lastCameraPositionX;
         lastCameraPositionX = currentCameraPositionX;
 
+        float cameraLeftEdge = currentCameraPositionX - cameraHalfWidth;
+        float cameraRightEdge = currentCameraPositionX + cameraHalfWidth;
+
         foreach (ParallaxLayer layer in backgroundLayers)
         {
             layer.Move(distanceToMove);
+            layer.LoopBackground(cameraLeftEdge, cameraRightEdge);
+        }
+    }
+    private void CalculateImageLength()
+    {
+        foreach (ParallaxLayer layer in backgroundLayers)
+        {
+            layer.CalculateImageWidth();
         }
     }
 }
